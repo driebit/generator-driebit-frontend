59b0440667eb3e63ca900b5a3cecfd66b42a2a01
Sjors Rijsdam <sjors@driebit.nl>
2015-05-21 11:46:06 +0200

    WIP

---
+++ app/index.js
@@ -0,0 +1,333 @@
+var generators = require('yeoman-generator'),
+    fs         = require('fs');
+
+module.exports = generators.Base.extend({
+    constructor: function () {
+        generators.Base.apply(this, arguments);
+    },
+
+    initializing: function () {
+        this.answers = {};
+
+        this.defaults = {
+            ginger: {
+                templateRoot: 'templates',
+                webRoot: 'lib'
+            },
+            symfony2: {
+                templateRoot: 'src/AppBundle/Resources/views',
+                webRoot: 'src/AppBundle/Resources/public'
+            }
+        };
+
+        this.log("     _      _      _     _ _");
+        this.log("  __| |_ __(_) ___| |__ (_) |_");
+        this.log(" / _` | '__| |/ _ \\ '_ \\| | __|");
+        this.log("| (_| | |  | |  __/ |_) | | |_");
+        this.log(" \\__,_|_|  |_|\\___|_.__/|_|\\__|");
+
+        // Probably Symfony2
+        if (fs.existsSync('src/AppBundle')) {
+            this.answers.stack        = 'symfony2';
+            this.answers.templateRoot = 'src/AppBundle/Resources/views';
+            this.answers.webRoot      = 'src/AppBundle/Resources/public';
+
+            this.log('[INFO] Possibly detected Symfony2 as backend technology');
+
+        // Probably Ginger
+        } else if (fs.existsSync('lib') && fs.existsSync('templates')) {
+            this.answers.stack        = 'ginger';
+            this.answers.templateRoot = 'templates';
+            this.answers.webRoot      = 'lib';
+
+            this.log('[INFO] Possibly detected Ginger as backend technology');
+        }
+    },
+
+    //
+    // PROMPT THE USER FOR VARIOUS OPTIONS
+    //
+    prompting: {
+        stack: function () {
+            var done = this.async(),
+                defaultValue;
+
+            this.prompt({
+                type: 'list',
+                name: 'stack',
+                message: 'What backend stack are you using?',
+                default: this.answers.stack,
+                choices: [
+                    { name: 'Symfony2 / Twig', value: 'symfony2' },
+                    { name: 'Ginger',          value: 'ginger' },
+                    { name: 'Other / Twig',    value: 'other' }
+                ]
+            }, (function (answer) {
+                this.answers.stack = answer.stack;
+
+                done();
+            }).bind(this))
+        },
+
+        templateRoot: function () {
+            var done = this.async(),
+                templateRoot;
+
+            if (this.answers.stack != 'other') {
+                templateRoot = this.defaults[this.answers.stack].templateRoot;
+            }
+
+            this.prompt({
+                type: 'input',
+                name: 'templateRoot',
+                message: 'What is the path of the template directory relative to the current directory (e.g. `views`)?',
+                default: templateRoot,
+            }, (function (answer) {
+                var path = answer.templateRoot;
+
+                this.answers.templateRoot = path.replace(/^\/|\/$/g, ''); // strip slashes at the beginning and the end of the path
+
+                done();
+            }).bind(this));
+        },
+
+        webRoot: function () {
+            var done = this.async(),
+                webRoot;
+
+            if (this.answers.stack != 'other') {
+                webRoot = this.defaults[this.answers.stack].webRoot;
+            }
+
+            this.prompt({
+                type: 'input',
+                name: 'webRoot',
+                message: 'What is the path of the web root relative to the current directory (e.g. `web`)?',
+                default: webRoot
+            }, (function (answer) {
+                var path = answer.webRoot;
+
+                this.answers.webRoot = path.replace(/^\/|\/$/g, ''); // strip slashes at the beginning and the end of the path
+
+                done();
+            }).bind(this));
+        },
+
+        libraries: function () {
+            var done = this.async();
+
+            this.prompt({
+                type: 'checkbox',
+                name: 'libraries',
+                message: 'Do you need any of the following libraries? You can add your own later if necessary',
+                choices: [
+                    { name: 'Angular',    value: 'angular' },
+                    { name: 'jQuery',     value: 'jquery',    checked: true },
+                    { name: 'HTML5 shiv', value: 'html5shiv', checked: true }
+                ],
+            }, (function (answers) {
+                this.answers.libraries = answers.libraries;
+
+                done();
+            }).bind(this));
+        },
+
+        features: function () {
+            var done = this.async();
+
+            this.prompt({
+                type: 'checkbox',
+                name: 'features',
+                message: 'What features do you need? If you are using Ginger, you don\'t need "asset versioning". Ginger takes care of that.',
+                choices: [
+                    //{ name: 'Asset versioning', value: 'assetversioning', checked: this.answers.stack !== 'ginger' },
+                    { name: 'LiveReload',       value: 'livereload',      checked: this.answers.stack !== 'ginger' }
+                ],
+            }, (function (answers) {
+                this.answers.features = answers.features;
+
+                done();
+            }).bind(this));
+        }
+    },
+
+    //
+    // WRITE ALL THE META DATA FILES
+    //
+    configuring:  {
+        jshint: function () {
+            var jshintSrc = this.templatePath('.jshintrc');
+
+            this.fs.copyTpl(
+                this.templatePath('.jshintrc'),
+                this.destinationPath('.jshintrc'),
+                { type: 'node' }
+            );
+
+            this.fs.copyTpl(
+                this.templatePath('.jshintrc'),
+                this.destinationPath(this.answers.webRoot + '/.jshintrc'),
+                {
+                    type: 'browser',
+                    jquery: this.answers.libraries.indexOf('jquery') > -1
+                }
+            );
+        },
+
+        editorConfig: function () {
+            this.fs.copy(
+                this.templatePath('.editorconfig'),
+                this.destinationPath('.editorconfig')
+            );
+        }
+    },
+
+    //
+    // WRITE THE PROJECT FILES
+    //
+    writing: {
+        layoutTemplate: function () {
+            var stack = this.answers.stack;
+                fileName;
+
+            if (stack == 'ginger') {
+                fileName = 'base.tpl';
+            } else {
+                fileName = 'layout.twig';
+            }
+
+            this.fs.copyTpl(
+                this.templatePath('baselayout.tmpl'),
+                this.destinationPath(this.answers.templateRoot + '/' + fileName),
+                {
+                    stack: stack,
+                    features: {
+                        livereload: this.answers.features.indexOf('livereload') > -1
+                    },
+                    libraries: {
+                        jquery: this.answers.libraries.indexOf('jquery') > -1,
+                        angular: this.answers.libraries.indexOf('angular') > -1,
+                        html5shiv: this.answers.libraries.indexOf('html5shiv') > -1
+                    }
+                }
+            );
+        },
+
+        gulpFile: function () {
+            this.fs.copyTpl(
+                this.templatePath('Gulpfile.js'),
+                this.destinationPath(),
+                {
+                    stack: stack,
+                    features: {
+                        livereload: this.answers.features.indexOf('livereload') > -1
+                    },
+                    libraries: {
+                        jquery: this.answers.libraries.indexOf('jquery') > -1,
+                        angular: this.answers.libraries.indexOf('angular') > -1,
+                        html5shiv: this.answers.libraries.indexOf('html5shiv') > -1
+                    }
+                }
+            );
+        },
+
+        cssStructure: function () {
+            var css = this.answers.webRoot + '/css';
+
+            fs.mkdirSync(css);
+            fs.mkdirSync(css + '/src');
+            fs.mkdirSync(css + '/src/modules');
+            fs.mkdirSync(css + '/src/pages');
+            fs.mkdirSync(css + '/dev');
+            fs.mkdirSync(css + '/prod');
+
+            this._.forEach([
+                'css/src/_mixins.scss',
+                'css/src/_variables.scss',
+                'css/src/screen.scss'
+            ], function (path) {
+                this.fs.copyTpl(
+                    this.templatePath(path),
+                    this.destinationPath(this.answers.webRoot + '/' + path)
+                );
+            });
+        },
+
+        jsStructure: function () {
+            var js = this.answers.webRoot + '/js';
+
+            fs.mkdirSync(js + '');
+            fs.mkdirSync(js + '/src');
+            fs.mkdirSync(js + '/prod');
+
+            if (this.answers.libraries.indexOf('angular') > -1) {
+                fs.mkdirSync(js + '/src/angular');
+            }
+        }
+    },
+
+    //
+    // INSTALL BOWER AND NPM MODULES
+    //
+    install: {
+        bower: function () {
+            var done      = this.async(),
+                libs      = this.answers.libraries,
+                completed = 0;
+
+            function installComplete () {
+                completed += 1;
+
+                if (completed === libs.length) {
+                    done();
+                }
+            }
+
+            this._.forEach(libs, function (lib) {
+                this.bowerInstall([lib], { 'save': true }, installComplete);
+            });
+        },
+
+        npm: function () {
+            var libsAll = [
+                    'gulp',
+                    'gulp-autoprefixer',
+                    'gulp-css-inline-images',
+                    'gulp-sass'
+                ],
+                libsLiveReload = [
+                    'gulp-livereload',
+                ],
+                libsNotGinger = [
+                    'gulp-concat',
+                    'gulp-minify-css',
+                    'gulp-uglify'
+                ],
+                installLibs,
+                completed = 0,
+                done = this.async();
+
+            function installComplete () {
+                completed += 1;
+
+                if (completed === installLibs.length) {
+                    done();
+                }
+            }
+
+            installLibs = libsAll;
+
+            if (this.stack != 'ginger') {
+                installLibs = installLibs.concat(libsNotGinger);
+            }
+
+            if (this.answers.features.indexOf('livereload') > -1) {
+                installLibs = installLibs.concat(libsLiveReload);
+            }
+
+            this._.forEach(installLibs, function (lib) {
+                this.npmInstall([lib], { 'save': true }, installComplete)
+            });
+        }
+    }
+});
---
+++ app/templates/Gulpfile.js
@@ -0,0 +1,137 @@
+var sass            = require('gulp-sass'),
+    autoprefixer    = require('gulp-autoprefixer'),
+    cssInlineImages = require('gulp-css-inline-images'),
+
+    <% if (features.livereload) { %>
+    livereload = require('gulp-livereload'),
+    <% } %>
+
+    <% if (stack == 'ginger') { %>
+    minifyCss = require('gulp-minify-css'),
+    <% } else { ?>
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    <% } %>
+
+    WEBROOT = '<%=paths.webRoot%>',
+    VIEWS = '<%=paths.views%>',
+
+    CSS      = WEBROOT + '/css',
+    CSS_SRC  = CSS + '/src',
+    CSS_DEV  = CSS + '/dev',
+    CSS_PROD = CSS + '/prod',
+
+    JS       = WEBROOT + '/js',
+    JS_SRC   = JS + '/src',
+    JS_DEV   = JS + '/dev',
+    JS_PROD  = JS + '/prod',
+
+    BOWER = WEBROOT + '/bower_components',
+    HTML5SHIV = BOWER + '/html5shiv/dist',
+
+    CSS_DEFAULT_OPS = [sass(), autoprefixer()];
+
+function process (input, output, operationss) {
+    var stream = gulp.src(input);
+
+    operationss.forEach(function (operation) {
+        stream = stream.pipe(operation);
+    });
+
+    return stream.pipe(gulp.dest(output));
+}
+
+gulp.task('watch', function () {
+    gulp.watch(CSS_SOURCE + '/**/*.scss', ['styles']);
+});
+
+<% if (features.livereload) { %>
+gulp.task('livereload', function () {
+    var server = livereload(),
+        glob;
+
+    gulp.watch([
+        CSS_DEV + '/*.css',
+        CSS_PROD + '/*.css',
+        <% if (stack == 'ginger') { %>
+        VIEWS + '/**/*.twig'
+        <% } else { %>
+        VIEWS + '/**/*.tpl'
+        <% } %>
+    ], function (file) {
+        server.changed(file.path);
+    });
+});
+<% } %>
+
+gulp.task('dev-css', function () {
+    return process(CSS_SRC, CSS_DEV, CSS_DEFAULT_OPS);
+});
+
+gulp.task('prod-css', function () {
+    var additionalOps = [],
+        allOps;
+
+    additionalOps.push(
+        cssInlineImages({
+            webRoot: WEBROOT
+        })
+    );
+
+    <% if (stack != 'ginger') { %>
+    additionalOps.push(
+        minifyCss({
+            keepSpecialComments: 0,
+            processImport: true,
+            noAdvanced: false,
+            root: WEBROOT,
+            relativeTo: CSS_PROD,
+            noRebase: true
+        })
+    );
+    <% } %>
+
+    allOps = CSS_DEFAULT_OPS.concat(additionalOps);
+
+    return process(CSS_SRC + '/*.scss', CSS_PROD, allOps);
+});
+
+<% if (stack != 'ginger') { ?>
+gulp.task('prod-js-html5shiv', function () {
+    return process(
+        [
+            HTML5SHIV + '/html5shiv.js',
+            HTML5SHIV + '/html5shiv-printshiv.js'
+        ],
+        JS_PROD,
+        [
+            concat('html5shiv.js'),
+            uglify()
+        ]);
+});
+<% } %>
+
+<% if (stack != 'ginger') { ?>
+<% if (libraries.jquery || libraries.angular) { ?>
+gulp.task('prod-js-vendors', function () {
+    var input = [];
+
+    <% if (libraries.jquery) { %>
+        input.push(BOWER + '/jquery/dist/jquery.js');
+    <% } %>
+
+    <% if (libraries.angular) { %>
+        input.push(BOWER + '/jquery/dist/angular.js');
+    <% } %>
+
+    return process(
+        input,
+        JS_PROD,
+        [
+            concat('bootstrap.js'),
+            uglify()
+        ]
+    );
+});
+<% } %>
+<% } %>
